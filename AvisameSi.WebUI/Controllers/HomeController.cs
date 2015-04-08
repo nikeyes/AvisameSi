7d289fc7f515a25fbaa25945cfd0869c570b6d95
jorge.castro <nikey_es@yahoo.es>
2015-04-09 01:13:08 +0200

    Inicio del proyecto

---
+++ AvisameSi.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AvisameSi.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+            IDatabase db = redis.GetDatabase();
+            string value = "abcdefg";
+            db.StringSet("mykey", value);
+            value = db.StringGet("mykey");
+            ViewBag.value = value;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ AvisameSi.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AvisameSi.WebUI.MvcApplication" Language="C#" %>
